bc2d2e124de06faca8424a2c82a66b0734238e3d
pipecm05 <andresf.garciac1@uqvirtual.edu.co>
2026-02-09 20:13:11 -0500

    listo para subir

--- src/main/java/co/edu/uniquindio/poo/RecorrerArreglo.java
+++ src/main/java/co/edu/uniquindio/poo/RecorrerArreglo.java
@@ -3,15 +3,12 @@ package co.edu.uniquindio.poo;
 public class RecorrerArreglo {
 
     public static void recorrer(int[] arreglo, int i) {
-
         // Caso base
         if (i == arreglo.length) {
             return;
         }
-
         // Acción
         System.out.println(arreglo[i]);
-
         // Llamada recursiva
         recorrer(arreglo, i + 1);
     }
--- src/main/java/co/edu/uniquindio/poo/RecorrerDiagonal.java
+++ src/main/java/co/edu/uniquindio/poo/RecorrerDiagonal.java
@@ -3,15 +3,12 @@ package co.edu.uniquindio.poo;
 public class RecorrerDiagonal {
 
     public static void recorrerDiagonal(int[][] matriz, int i) {
-
         // Caso base
         if (i == matriz.length) {
             return;
         }
-
         // Acción
         System.out.println(matriz[i][i]);
-
         // Llamada recursiva
         recorrerDiagonal(matriz, i + 1);
     }
